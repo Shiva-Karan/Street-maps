194912d3675bf243e1ee43d40cec838175290d05
lucky3aeon <lucky3aeon@yahoo.com>
2017-06-16 17:41:46 +0530

    files uploaded
    except location everything else works

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/*eslint-disable*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "antd/dist/antd.css";
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import * as firebase from 'firebase';
+
+
+const config = {
+    apiKey: "AIzaSyBuDnDoFr__voyw_jqrZof9W3lay7sSWI4",
+    authDomain: "street-41cb0.firebaseapp.com",
+    databaseURL: "https://street-41cb0.firebaseio.com",
+    projectId: "street-41cb0",
+    storageBucket: "street-41cb0.appspot.com",
+    messagingSenderId: "888555847354"
+  };
+  firebase.initializeApp(config);
+
+
+ReactDOM.render(
+    <div style={{width: '100%', height: '400px'}}>
+        <App />
+    </div>, 
+    document.getElementById('root')
+);
+
